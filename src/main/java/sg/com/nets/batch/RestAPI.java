338610467d598fd22b8f55228722ae26598c95a7
coldtwister <coldtwister09@gmail.com>
2021-04-25 22:43:13 +0800

    Adding exception handling

--- src/main/java/sg/com/nets/batch/RestAPI.java
+++ src/main/java/sg/com/nets/batch/RestAPI.java
@@ -8,6 +8,11 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -15,11 +20,19 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import sg.com.nets.batch.config.StepExecListener;
 
 @RestController
 @RequestMapping("/api")
@@ -34,6 +47,12 @@ public class RestAPI {
 	
 	@Autowired
 	JobLauncher jobLauncher;
+	
+	@Autowired
+	StepExecListener stepExecutionListener;
+	
+	@Autowired
+	StepBuilderFactory stepBuilderFactory;
 	
 	@GetMapping("batch")
 	public String testJob() {
@@ -56,7 +75,7 @@ public class RestAPI {
 			Step step = (Step) context.getBean(s);
 			sjb.next(step);
 		}
-		
+		sjb.next(this.createStep("customStep2"));
 		
 		Job job = sjb.repository(jobRepository).build();
 		
@@ -66,10 +85,53 @@ public class RestAPI {
 			e.printStackTrace();
 		} 
 		
-		return "BATCH COMPELTED";
+		return "BATCH COMPLETED";
 	}
 	
 	
+	@StepScope
+	public Step createStep(String stepName) {
+		/*
+		 * StepBuilder stepBuilder = new StepBuilder(stepName);
+		 * stepBuilder.tasklet(createTaskLet()).build().setJobRepository(jobRepository);
+		 */
+		
+		return stepBuilderFactory.get(stepName).listener(new StepExecListener()).tasklet((Tasklet) context.getBean(stepName)).build();
+		
+	}
 	
+	@StepScope
+	@Bean("customStep1")
+	public Tasklet createTaskLet1() {
+		return new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("Completed....." + chunkContext.getStepContext().getStepName());
+//					if(true) {
+//						throw new Exception("I demand some error...");
+//					}
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
+	
+	@StepScope
+	@Bean("customStep2")
+	public Tasklet createTaskLet2() {
+		return new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("Completed....." + chunkContext.getStepContext().getStepName());
+					
+//					if(true) {
+//						throw new Exception("I demand some error...");
+//					}
+					
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
 
 }
